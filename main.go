2e67c9a52abc5ded1a0f1ae459af9f2d9aecd77f
Ameer Deen <ameer.deen@qigroup.com>
2024-12-28 08:57:12 +0530

    testing git id

--- main.go
+++ main.go
@@ -5,16 +5,4 @@ func main() {
 	dStream := NewServer()
 	dStream.Start()
 
-	// config := config.NewConfig()
-
-	// duration := time.Duration(time.Second * 60)
-	// locker, err := lockers.NewBlobLocker(config.Locks.ConnectionString, config.Locks.ContainerName, "deleteme", duration)
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
-
-	// locker.GetLockedTables()
-	// // locker.AcquireLock()
-	// // locker.ReleaseLock()
-
 }
