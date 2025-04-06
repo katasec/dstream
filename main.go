074626be9012b292ff2fbef2601fe71e6a417be9
Ameer Deen <writeameer@gmmail.com>
2025-04-06 17:35:07 +0400

    Removed extra test logs

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 
 import (
 	"github.com/katasec/dstream/cmd"
-	"github.com/katasec/dstream/internal/logging"
 )
 
 func main() {
-	logger := logging.GetLogger()
-	logger.Warn("This is a warning message")
 	cmd.Execute()
 }
