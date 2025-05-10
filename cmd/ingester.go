c08245b9cc7fff9ebfd5a6a409bae9d6f733fad5
Ameer Deen <writeameer@gmmail.com>
2025-05-10 22:29:37 +0400

    Working on plugin model

--- cmd/ingester.go
+++ cmd/ingester.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	mssql "github.com/katasec/dstream-ingester-mssql/mssql"
-	"github.com/katasec/dstream/pkg/logging"
 	"github.com/katasec/dstream/pkg/plugins"
 	"github.com/spf13/cobra"
 )
@@ -15,12 +14,10 @@ var ingesterCmd = &cobra.Command{
 	Short: "Start the DStream ingester",
 	Long:  `Start the DStream ingester which monitors SQL Server tables for changes using Change Data Capture (CDC) and sends them to the ingest queue.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		logger := logging.GetLogger()
 		ing := mssql.New()
 		ctx := context.Background()
 
 		err := ing.Start(ctx, func(e plugins.Event) error {
-			logger.Info("Received event: %+v", e)
 			return nil
 		})
 
