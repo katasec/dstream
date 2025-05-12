a6c88ccd2a9e2623060498150d604af67318299a
Ameer Deen <writeameer@gmmail.com>
2025-05-12 07:44:35 +0400

    updated script for testing sample provider

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 
-go build -o dstream-ingester-time ./cmd/dstream-ingester-time
+go build -o out/dstream-ingester-time ./cmd/dstream-ingester-time
 go run ./cmd/run-time-plugin/main.go
